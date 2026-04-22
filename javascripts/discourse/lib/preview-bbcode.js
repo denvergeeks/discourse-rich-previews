ee2b41d7da8e6488c77ae4e7883182c6f05b4249
denvergeeks <jen@denvergeeks.com>
2026-04-22 07:55:40 -0600

    Update preview-bbcode.js

--- javascripts/discourse/lib/preview-bbcode.js
+++ javascripts/discourse/lib/preview-bbcode.js
@@ -103,7 +103,16 @@ function stampAutoLinkIndicators(root, config) {
       link.style.setProperty("--rp-color", color);
     }
 
-    link.style.removeProperty("--rp-icon");
+    const iconMap = {
+      topic: config?.previewsIconTopic || "*",
+      external: config?.previewsIconExternal || "+",
+      wikipedia: config?.previewsIconWikipedia || "#",
+    };
+
+    const icon = iconMap[type];
+    if (icon) {
+      link.style.setProperty("--rp-icon", `"${icon}"`);
+    }
   });
 }
 
